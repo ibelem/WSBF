1a0d85adfb42467e374bcd2a825a3b26b73b4a29
Belem Zhang <belem.zhang@intel.com>
2017-05-12 10:54:45 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'WSBF! C'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'WSBF string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'WSBF json'
+  }
+})
+
+module.exports = router
